f442e7fa10641e47fbeab5a39a569083ebad3847
Meehoi <1791353+beam41@users.noreply.github.com>
2026-03-26 00:06:27 +0700

    add .d.ts
    update version
    
    test

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import { UserConfig } from "@unocss/core";
+import { Theme as PresetWind4Theme } from "@unocss/preset-wind4";
+
+declare const config: UserConfig<PresetWind4Theme>;
+export default config;
